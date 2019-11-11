0eb2ddb041ca295cefdd0d86e54ffffcd5051953
Wladislaw <wladikowalew@gmail.com>
2019-11-11 12:25:11 +0300

    add all files

---
+++ Table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by Student on 22/10/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
